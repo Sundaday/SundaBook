8b3c499668cd3a10cc4e2697970583563593a7e4
Sundaday <john.koch.pro@gmail.com>
2022-08-01 23:10:18 +0200

    init project

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@sundacluster0.vvmwg.mongodb.net/SundaBook",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.log('Failed to connect to MongoDB', err))
