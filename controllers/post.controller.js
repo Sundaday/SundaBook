4a4e0c1b7b45663fc93eed89d9930c1ee299427a
Sundaday <john.koch.pro@gmail.com>
2022-08-11 16:13:37 +0200

    add post model/routes/controllers

---
+++ controllers/post.controller.js
@@ -0,0 +1,8 @@
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectId = require("mongoose").Types.ObjectId;
+
+module.exports.createPost = (req, res) => {};
+module.exports.readPost = (req, res) => {};
+module.exports.updatePost = (req, res) => {};
+module.exports.deletePost = (req, res) => {};
