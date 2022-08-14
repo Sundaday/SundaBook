c808d6439fec464c2574ab355104f8b2745255ff
Sundaday <john.koch.pro@gmail.com>
2022-08-14 21:41:47 +0200

    add upload controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,17 +2,19 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
 
-//auth
+//auth user
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logout);
 
-//get User display
+//crud user
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
+
+//upload
 
 module.exports = router;
