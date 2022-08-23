d0c4006842507046e6829e18151353c708d892c4
Sundaday <john.koch.pro@gmail.com>
2022-08-23 18:52:13 +0200

    set check requireAuth JWT in 2ndhightest scope location

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Routes from './components/Routes'
+import { UidContext } from './components/AppContext'
+import axios from 'axios'
 
 export default function App() {
+  const [uid, setUid] = useState()
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => 
+          {
+            console.log(res)
+            setUid(res.data)
+          })
+        .catch((err) => console.log('No token found'))
+    };
+    fetchToken();
+  }, [uid]) // []<---- named callbacks for avoid circular dependency issues
+  // [uid] => refresh value for uid
+
   return (
-    <Routes />
+    <UidContext.Provider value={uid}>
+      <Routes />
+    </UidContext.Provider>
   )
 }
