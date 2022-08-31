4383f847f6b83953e3d8f4f67009eba99baa7880
Sundaday <john.koch.pro@gmail.com>
2022-08-31 23:31:40 +0200

    add form for upload img + add uploadpicture & user.actions + add case to reducer

--- client/src/actions/user.actions.js
+++ client/src/actions/user.actions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const GET_USER = "GET_USER";
+export const UPLOAD_PICTURE = "UPLOAD_PICTURE";
+
 export const getUser = (uid) => {
     return (dispatch) => {
         return axios
@@ -11,3 +13,18 @@ export const getUser = (uid) => {
             .catch((err) => console.log(err))
     }
 };
+
+export const uploadPicture = (data, id) => {
+    return (dispatch) => {
+        return axios
+            .post(`${process.env.REACT_APP_API_URL}api/user/upload`, data)
+            .then((res) => {
+                return axios
+                    .get(`${process.env.REACT_APP_API_URL}api/user/${id}`)
+                    .then((res) => {
+                        dispatch({ type: UPLOAD_PICTURE, payload: res.data.picture })
+                    })
+            })
+            .catch((err) => console.log(err))
+    }
+}
--- client/src/components/Profil/UploadImg.js
+++ client/src/components/Profil/UploadImg.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { uploadPicture } from '../../actions/user.actions';
 
 export default function UploadImg() {
-  return (
-    <div>UploadImg</div>
-  )
+    const [file, setFile] = useState()
+    const dispatch = useDispatch();
+    const userData = useSelector((state) => state.userReducer)
+
+    const handlePicture = (e) => {
+        const data = new FormData();
+        data.append('name', userData.pseudo)
+        data.append('userId', userData._id)
+        data.append('file', file)
+
+        dispatch(uploadPicture(data, userData._id))
+     }
+
+    return (
+        <form onSubmit={handlePicture} className='upload-pic'>
+            <label hidden='file'>Select new Image</label>
+            <input
+                type='file'
+                id='file'
+                name='file'
+                accept='.jpg, .png, .jpeg'
+                onChange={(e) => setFile(e.target.files[0])}
+            />
+            <br />
+            <input type='submit' value='Upload' />
+        </form>
+    )
 }
