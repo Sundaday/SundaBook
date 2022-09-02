09a654aa7e86ba325a780386ca097c461f98c7e2
Sundaday <john.koch.pro@gmail.com>
2022-09-02 16:08:02 +0200

    add isEmpty func in Utils + use it into FollowHandler to manage button

--- client/src/components/Profil/FollowHandler.js
+++ client/src/components/Profil/FollowHandler.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { isEmpty } from '../Utils'
 
 export default function FollowHandler({ idToFollow }) {
     const userData = useSelector((state) => state.userReducer)
     const [isFollowed, setIsFollowed] = useState(false)
 
     const handleFollow = () => {
-        
+
     }
     const handleUnfollow = () => {
 
     }
 
     useEffect(() => {
-
+        if (!isEmpty(userData.following)) {
+            if (userData.following.includes(idToFollow)) {
+                setIsFollowed(true);
+            } else setIsFollowed(false);
+        }
     }, [userData, idToFollow]) // []<= callback to restart useEffect
 
     return (
-        <div>Follow</div>
+        <>
+            {isFollowed && (
+                <span>
+                    <button className='unfollow-btn'>Subscriber</button>
+                </span>
+            )}
+            {isFollowed === false && (
+                <span>
+                    <button className='follow-btn'>Follow</button>
+                </span>
+            )}
+        </>
     )
 }
